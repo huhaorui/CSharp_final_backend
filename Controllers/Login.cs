c255a758edfa07cb7c91fef0f38137100c715e17
HHR <i@huhaorui.com>
2020-11-06 19:49:31 +0800

    实现基础用户功能

---
+++ Controllers/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/user/login")]
+    [ApiController]
+    public class Login : ControllerBase
+    {
+        private readonly ILogger<Login> _logger;
+
+        public Login(ILogger<Login> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public String Post(string uid, string password)
+        {
+            var connection = Connection.GetConn();
+            var Sql = "select * from User where uid=@uid and password=@password";
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            command.Parameters.Add(new MySqlParameter("@uid", uid));
+            command.Parameters.Add(new MySqlParameter("@password", password));
+            connection.Open();
+            var result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                connection.Close();
+                return "OK";
+            }
+
+            connection.Close();
+            return "NO";
+        }
+    }
+}
---
+++ Controllers/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/user/register")]
+    [ApiController]
+    public class Register : ControllerBase
+    {
+        private readonly ILogger<Register> _logger;
+
+        public Register(ILogger<Register> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public String Post(string uid, string password)
+        {
+            var connection = Connection.GetConn();
+            var Sql = "insert into User (uid,password) values (@uid,@password)";
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            command.Parameters.Add(new MySqlParameter("@uid", uid));
+            command.Parameters.Add(new MySqlParameter("@password", password));
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            return "OK";
+        }
+    }
+}
