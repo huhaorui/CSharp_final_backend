9b755ab65734bf4671f6ec2168b67b5dd4c48461
HHR <i@huhaorui.com>
2020-11-07 02:44:28 +0800

    完成游戏准备的几个接口

---
+++ Controllers/Ready.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Renci.SshNet.Messages.Connection;
+
+namespace backend.Controllers
+{
+    [Route("api/game/ready")]
+    [ApiController]
+    public class Ready : ControllerBase
+    {
+        private readonly ILogger<Ready> _logger;
+
+        public Ready(ILogger<Ready> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public String Post([FromForm] string uid, [FromForm] string password, [FromForm] string attribute)
+        {
+            if (!(attribute.Equals("ready") || attribute.Equals("unready")))
+            {
+                return "WA";
+            }
+
+            var cmd = attribute.Equals("ready") ? "1" : "0";
+            var connection = Connection.GetConn();
+            var Sql = "select did from AllView where uid=@uid and password=@password";
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            command.Parameters.Add(new MySqlParameter("@uid", uid));
+            command.Parameters.Add(new MySqlParameter("@password", password));
+            connection.Open();
+            var result = command.ExecuteReader();
+            if (result.Read())
+            {
+                var did = result.GetInt32("did").ToString();
+                result.Close();
+                Sql = "select player1,player2,ready from Desk where did=@did";
+                command = new MySqlCommand(Sql, connection);
+                command.Parameters.Add(new MySqlParameter("@did", did));
+                result = command.ExecuteReader();
+                if (result.Read())
+                {
+                    Sql = "update Desk set ready=@ready where did=@did";
+                    command = new MySqlCommand(Sql, connection);
+                    command.Parameters.Add(new MySqlParameter("@did", did));
+                    if (uid.Equals(result.GetInt32("player1").ToString()))
+                    {
+                        command.Parameters.Add(new MySqlParameter("@ready", cmd + result.GetString("ready")[1]));
+                    }
+                    else if (uid.Equals(result.GetInt32("player2").ToString()))
+                    {
+                        command.Parameters.Add(new MySqlParameter("@ready", result.GetString("ready")[0] + cmd));
+                    }
+                    result.Close();
+                    command.ExecuteNonQuery();
+                    return "OK";
+                }
+
+                return "ERROR";
+            }
+
+            connection.Close();
+            return "NO";
+        }
+    }
+}
---
+++ Controllers/Status.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/game/status")]
+    [ApiController]
+    public class Status : ControllerBase
+    {
+        private readonly ILogger<Status> _logger;
+
+        public Status(ILogger<Status> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public string Post([FromForm] string uid, [FromForm] string password)
+        {
+            var connection = Connection.GetConn();
+            var sql = "select status,did,next,ready from AllView where uid=@uid and password=@password";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.Parameters.Add(new MySqlParameter("@uid", uid));
+            command.Parameters.Add(new MySqlParameter("@password", password));
+            connection.Open();
+            var result = command.ExecuteReader();
+            var response = "";
+            if (result.Read())
+            {
+                response += result.GetInt32("did");
+                response += ":";
+                response += result.GetString("status");
+                response += ":";
+                response += result.GetInt32("next");
+                response += ":";
+                response += result.GetString("ready");
+            }
+            else
+            {
+                response += "NG"; //No Game
+            }
+
+            result.Close();
+            return response;
+        }
+    }
+}
