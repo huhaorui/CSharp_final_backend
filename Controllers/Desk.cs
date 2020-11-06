91973e8933912ec6540c140829540c417ad020fb
HHR <i@huhaorui.com>
2020-11-06 21:40:30 +0800

    完成游戏桌有关接口

---
+++ Controllers/Desk.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/desk/list")]
+    [ApiController]
+    public class Desk : ControllerBase
+    {
+        private readonly ILogger<Desk> _logger;
+
+        public Desk(ILogger<Desk> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public String Post()
+        {
+            var connection = Connection.GetConn();
+            var Sql = "select * from Desk";
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            connection.Open();
+            var result = command.ExecuteReader();
+            var response = "";
+            while (result.Read())
+            {
+                int did = 0, player1 = 0, player2 = 0, gid = 0;
+                if (!result.IsDBNull(0))
+                    did = result.GetInt32("did");
+                if (!result.IsDBNull(1))
+                    player1 = result.GetInt32("player1");
+                if (!result.IsDBNull(2))
+                    player2 = result.GetInt32("player2");
+                if (!result.IsDBNull(3))
+                    gid = result.GetInt32("gid");
+                response += $"{did} {player1} {player2} {gid}\n";
+            }
+
+            connection.Close();
+            Console.WriteLine(response);
+            return response;
+        }
+    }
+}
---
+++ Controllers/EnterDesk.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace backend.Controllers
+{
+    [Route("api/desk/enter")]
+    [ApiController]
+    public class EnterDesk : ControllerBase
+    {
+        private readonly ILogger<EnterDesk> _logger;
+
+        public EnterDesk(ILogger<EnterDesk> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public String Post(String uid, String password, int desk, int seat, String attribute)
+        {
+            var connection = Connection.GetConn();
+            var sql = "select * from User where uid=@uid and password=@password";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.Parameters.Add(new MySqlParameter("@uid", uid));
+            command.Parameters.Add(new MySqlParameter("@password", password));
+
+            connection.Open();
+            var result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                result.Close();
+                if (attribute.Equals("sitdown"))
+                {
+                    sql = seat == 1
+                        ? "update Desk set player1=@uid where did=@desk"
+                        : "update Desk set player2=@uid where did=@desk";
+                    command = new MySqlCommand(sql, connection);
+                    command.Parameters.Add(new MySqlParameter("@uid", uid));
+                    command.Parameters.Add(new MySqlParameter("@desk", desk));
+                }
+                else
+                {
+                    sql = seat == 1
+                        ? "update Desk set player1=null where player1=@uid"
+                        : "update Desk set player2=null where player2=@uid";
+                    command = new MySqlCommand(sql, connection);
+                    command.Parameters.Add(new MySqlParameter("@uid", uid));
+                }
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    return "OK";
+                }
+                else
+                {
+                    return "SH"; //somebody here
+                }
+            }
+            else
+            {
+                return "PE"; //password error
+            }
+        }
+    }
+}
